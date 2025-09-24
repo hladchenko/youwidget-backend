63ed10f967e079d6396dc52cc77bb0371d6de2f5
Ruslan Hladchenko <rugladchenko@gmail.com>
2025-09-24 23:41:18 +0300

    Update port

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const fastify = Fastify({
 
 fastify.register(cors, {
   origin: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 });
 
 fastify.register(dbConnector);
@@ -17,8 +17,11 @@ fastify.register(widgetRoutes, { prefix: "/widgets" });
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 8080 });
-    console.log("Server running on http://localhost:8080");
+    await fastify.listen({
+      port: Number(process.env.PORT) || 8080,
+      host: "0.0.0.0",
+    });
+    console.log(`Server running on port ${process.env.PORT || 8080}`);
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
