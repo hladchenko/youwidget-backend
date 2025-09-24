b17964b3bd341578db466c06d94d996bc0dabca6
Ruslan Hladchenko <rugladchenko@gmail.com>
2025-09-24 23:52:24 +0300

    Add database url from env

--- src/plugins/dbConnector.ts
+++ src/plugins/dbConnector.ts
@@ -3,7 +3,7 @@ import { DatabaseSync } from "node:sqlite";
 import type { FastifyInstance } from "fastify";
 
 async function dbConnector(fastify: FastifyInstance) {
-  const database = new DatabaseSync("db.sqlite");
+  const database = new DatabaseSync(process.env.DATABASE_URL || "db.sqlite");
 
   database.exec(`
     CREATE TABLE IF NOT EXISTS widgets (
